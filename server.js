2e2330e88747e4972f0cf2ccb3907dfbf97985ba
Ruben V <ralvuurman@gmail.com>
2021-09-30 16:49:40 +0200

    Register & Login functie
    Ik heb de backend opgezet en ben begonnen met de registreer & login functie. Deze werken nu maar er zit bijna tot geen beveiliging nog op.

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-app.get("/", function(req, res) {
-    res.send("Express here")
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,
+    optionSuccessStatus:200
+}
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// Database informatie
+const db = require('./database');
+
+app.get('/', (req, res) => res.send('Test'));
+
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(
+        "INSERT INTO users (username, password) VALUES (?,?)",
+        [username, password],
+        (err, result) => {
+            console.log(err);
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        [username, password],
+        (err, result) => {
+            if (err) {
+                res.send({err: err});
+            }  
+            if (result.length > 0) {
+                res.send(result);
+            } else {
+                res.send({message: "Het opgegeven emailadres bestaat niet of het wachtwoord is incorrect."});
+            }
+        }
+    );
 })
 
 app.listen(3001, function() {
     console.log("Express server running on port 3001")
-})
+});
