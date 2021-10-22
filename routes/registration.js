14c2a04ba90e615d3a57af30ff0d0ec90ed9f292
Ruben V <ralvuurman@gmail.com>
2021-10-22 16:40:00 +0200

    Gebruikersrollen
    Gebruikers krijgen nu een rol toegewezen aan hun account, dit zal later van pas komen

--- routes/registration.js
+++ routes/registration.js
@@ -7,6 +7,7 @@ module.exports = function(express, app){
     app.post('/register', (req, res) => {
         const username = req.body.username;
         const password = req.body.password;
+        const role = req.body.role;
         
         bcrypt.hash(password, saltRounds, (err, hash) => {
             if (err) {
@@ -15,8 +16,8 @@ module.exports = function(express, app){
             
             // Database query om de gegevens van de gebruiker in de database te zetten
             db.query(
-                "INSERT INTO users (username, password) VALUES (?,?)",
-                [username, hash],
+                "INSERT INTO users (username, password, role) VALUES (?,?,?)",
+                [username, hash, role],
                 (err, result) => {
                     console.log(err);
             });  
